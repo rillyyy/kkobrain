127885b0747a5c190ebbcca47730645cf8991f6e
rillyyy <https://github.com/rillyyy/react-study.git>
2024-03-26 13:00:08 +0900

    Temporary commit before pulling changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <meta name="theme-color" content="#000000" />
     <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
     <meta name="google-site-verification" content="8R3hdkwyAzSTKAqAE00hrP5jBrzy22IApyWWf5SAyKI" />
-    <title>카카오 브레인 블로그 - 클론코드</title>
+    <title>카카오 브레인 블로그  -  클론코드</title>
   </head>
   <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>

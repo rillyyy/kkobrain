67820446372543352e07578719be88c0476dbdac
rillyyy <https://github.com/rillyyy/react-study.git>
2024-03-27 10:03:34 +0900

    GA4 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 // index.html 역할
 
 import { Metadata } from "next";
@@ -12,19 +13,22 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
 <html lang="ko">
-        
   <head>
-    <meta name="theme-color" content="#000000" />
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <meta name="google-site-verification" content="8R3hdkwyAzSTKAqAE00hrP5jBrzy22IApyWWf5SAyKI" />
-    <title>카카오 브레인 블로그  -  클론코드</title>
+    <meta
+      name="naver-site-verification"
+      content="18932c563ca8c7600147e0f60244307d7b421797"
+    />
+    <link rel="canonical" href="https://kkobrain-six.vercel.app"></link>
+    <meta
+      name="google-site-verification"
+      content="Q2bHOLs6jaFYy4NK_gCCdUCjf270wQ8zYE_nerDd7lw"
+    />
+    <GoogleAnalytics gaId="G-F8F7K7EMRX" />
   </head>
   <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div id="root">{children}</div>
   </body>
 </html>
-
   );
 }
- 
